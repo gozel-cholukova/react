02cc6b3873060a74e63394e0f4c5c894fff3a9ae
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-11-24 23:18:13 +0500

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { ReactComponent as PlusIcon } from './icons/plus.svg';
 import { ReactComponent as CogIcon } from './icons/cog.svg';
 import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
 import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
-import { ReactComponent as BoltIcon } from './icons/bolt.svg';
+
 
 import React, { useState, useEffect, useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
