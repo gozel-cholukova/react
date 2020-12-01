d8617fa8c7cc58d923d0adca12bcab853495f20a
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-12-01 10:12:37 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 import Movie from './components/Movie';
 
 const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 function App() {
-  const [ movies, setMovies ] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
       fetch(FEATURED_API)
@@ -18,11 +17,12 @@ function App() {
           });
   }, []);
 
-  return <div>
-    {movies.length > 0 && movies.map((movie) => 
-      <Movie key={movie.id} data={movie}></Movie>
-    )}
-    </div>;
+  return (
+      <div>
+        {movies.length > 0 && 
+            movies.map((movie) => <Movie key={movie.id} {...movie} /> )}
+      </div>
+  )
 }
 
 export default App;
