ef383f5343680655892c227b77608f3c47ee9ec8
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-12-03 00:59:55 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,22 +6,56 @@ const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popula
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 function App() {
-  const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
-  useEffect(() => {
-      fetch(FEATURED_API)
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data);
-            setMovies(data.results);
-          });
-  }, []);
+    useEffect(() => {
+        fetch(FEATURED_API)
+            .then((res) => res.json())
+            .then((data) => {
+              setMovies(data.results);
+            });
+    }, []);
+
+    const getMovies (API) {
+
+    }
+
+    const handleOnSubmit = (e) => {
+      e.preventDefault();
+
+      if (searchTerm) {
+          fetch(SEARCH_API + searchTerm)
+                .then((res) => res.json())
+                .then((data) => {
+                  setMovies(data.results);
+                });
+                setSearchTerm("");
+      }
+    };
+
+    const handleOnChange = (e) => {
+      setSearchTerm(e.target.value);
+    };
 
   return (
-      <div>
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}></form>
+          <input 
+            className="search" 
+            type="search" 
+            placeholder="Search..." 
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+      </header>
+
+      <div className="movie-container">
         {movies.length > 0 && 
-            movies.map((movie) => <Movie key={movie.id} {...movie} /> )}
+            movies.map((movie) => <Movie key={movie.id} {...movie}></Movie> )}
       </div>
+    </>
   )
 }
 
