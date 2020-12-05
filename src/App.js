1fe44c21b3da50fa73e8b6792b26788cb2b4eb52
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-12-05 22:52:29 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,56 +7,54 @@ const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a
 
 function App() {
     const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState("");
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
-        fetch(FEATURED_API)
-            .then((res) => res.json())
-            .then((data) => {
-              setMovies(data.results);
-            });
+      getMovies(FEATURED_API);
     }, []);
-
-    const getMovies (API) {
-
+    
+    const getMovies = (API) => {
+        fetch(API)
+        .then((res) => res.json())
+        .then((data) => {
+          setMovies(data.results);
+        });
     }
 
     const handleOnSubmit = (e) => {
       e.preventDefault();
 
       if (searchTerm) {
-          fetch(SEARCH_API + searchTerm)
-                .then((res) => res.json())
-                .then((data) => {
-                  setMovies(data.results);
-                });
-                setSearchTerm("");
+          getMovies(SEARCH_API + searchTerm);
+             
+            setSearchTerm("");
       }
     };
 
     const handleOnChange = (e) => {
       setSearchTerm(e.target.value);
-    };
+    }
 
-  return (
-    <>
-      <header>
-        <form onSubmit={handleOnSubmit}></form>
-          <input 
-            className="search" 
-            type="search" 
-            placeholder="Search..." 
-            value={searchTerm}
-            onChange={handleOnChange}
-          />
-      </header>
+    return (
+      <>
+        <header>
+          <form onSubmit={handleOnSubmit}>
+            <input 
+              className="search" 
+              type="search" 
+              placeholder="Search..." 
+              value={searchTerm}
+              onChange={handleOnChange}
+            />
+          </form>
+        </header>
 
-      <div className="movie-container">
-        {movies.length > 0 && 
-            movies.map((movie) => <Movie key={movie.id} {...movie}></Movie> )}
-      </div>
-    </>
-  )
+        <div className="movie-container">
+          {movies.length > 0 && 
+              movies.map((movie) => <Movie key={movie.id} {...movie}></Movie> )}
+        </div>
+      </>
+    );
 }
 
 export default App;
